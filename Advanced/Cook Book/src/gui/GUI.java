b5df2d836c355a6052b6d26297cb0cbd34d45e87
joshsizer <joshsizer@gmail.com>
2016-05-17 13:08:09 -0400

    Recipe Panel is done

--- Advanced/Cook Book/src/gui/GUI.java
+++ Advanced/Cook Book/src/gui/GUI.java
@@ -4,13 +4,14 @@ import java.awt.Dimension;
 
 public class GUI {
 	private Frame frame;
-	protected static int perferredHeight = 720;
-	protected static int perferredWidth = 1280;
+	protected static int perferredHeight = 480;
+	protected static int perferredWidth = 640;
 	protected static Dimension perferredSize = new Dimension(
 			perferredWidth, perferredHeight);
 	
 	public GUI() {
-		frame = new Frame();	
+		frame = new Frame();
+		frame.setResizable(false);
 	}
 	
 	public void display() {
